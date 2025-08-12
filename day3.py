25d389653a0bcb8d4721c8f99e3b82bbbcd28379
Harikrishnan <54831389+harikriz22@users.noreply.github.com>
2025-08-12 12:15:24 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,113 @@
+# strings
+
+# sequence of charectors ' ""
+
+
+# a = "hari's resume"
+# location = "kochi"
+# address = '''kakanad'''
+
+# escape sequence \
+# \' -- '
+# \" --- "
+# \n -- newline
+# \b - backspace
+# \t - tabspace
+# \\ - \
+
+# a = "ha\\trisresume"
+# print(a)
+
+
+# type conversion
+
+
+# 2 type
+# implicit
+# explicit
+
+# a = '5'
+
+# print(type(a))
+
+# a = 5
+
+# c,c++,java - statically typed language
+
+# python dynamically type language
+
+# run time type recognised
+
+# explicit conversion
+
+# b = float(5)
+# c = str(True)
+# print(c)
+
+# raw str
+
+# a = r"fghsdfhsd\tsgdfuhsuidf\nfhsdiufh "
+# print(a)
+# rawstring
+# string formating
+
+# string operations
+# input function
+# indexing
+
+
+# name = 'harikrishnan'
+# age = 28
+# rating = 9.9
+# married = False
+
+# status = f"name is {name} age is {age} rating is {rating} is married - {married}"
+
+# print(status)
+
+
+# arithemetic operators
+# boolean operators
+
+# string operations
+
+# 1.str concationation (addition)
+
+# a = 'hari'
+# b = 'krishnan'
+# print(a+b)
+
+# multiplication
+
+# a = 'hari'
+# b = 5
+# print(a*b)
+
+# membership operator   in operator
+
+# name = 'hari'
+
+# print('hi' in name)
+
+# input
+
+# a = int(input('enter a value:- '))
+# b = int(input('enter a value:- '))
+# print(b)
+# print(a+b)
+# ip - 
+# fname = input('enter first name : - ')
+# mname = input('enter middle name : -')
+# lname = input('enter last name : - ')
+
+# print(f'full name is {fname} {mname} {lname}')
+# full name is 
+
+# indexing 
+
+# name = input
+# print(name[10])
+
+# sum and average of 5 
+a = int(input('enter a value:- '))
+
